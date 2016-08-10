e41133488dca7e6f9953bec68f0a45e5c24f6d3e
Peter Chaula <petercchaula@gmail.com>
2016-08-10 17:14:00 +0200

    Shop methods

--- app/Shop.php
+++ app/Shop.php
@@ -10,4 +10,30 @@ class Shop extends Model
     {
         return $this->hasMany('App\Log');
     }
+
+    /**
+     * @return bool
+     */
+    public function isInstalled()
+    {
+        return $this->app_installed === 1;
+    }
+
+    /**
+     *Shop was installed then uninstalled
+     *
+     * @return bool
+     */
+    public function hasBeenInstalled()
+    {
+        return $this->app_installed === 2;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasNeverBeenInstalled()
+    {
+        return $this->app_installed == 0;
+    }
 }
