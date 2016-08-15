b37fc7fe4cb5d374f6e71a254ea3099da9eca8b8
Peter Chaula <petercchaula@gmail.com>
2016-08-15 16:10:07 +0200

    Fix views

--- resources/views/app/install.blade.php
+++ resources/views/app/install.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
-
+@section('title', 'Install app')
 @section('content')
     <section class="one-third">
         <div class="card">
             <form action="{{url('/install')}}" class="" method="post">
                 {{ csrf_field() }}
+                <div class="column three">&nbsp;</div>
                 <div class="align-center">
-                    <div class="input-group column six" style="float: none; margin: auto;">
+                    <div class="input-group column six {{Session::has('shop_error') ? 'error'  : ''}}">
                         <input type="text" name="shop" placeholder="yourshop.myshopify.com" value="{{$shop}}">
                         <button>Install</button>
                     </div>
                 </div>
+                <div class="column three">&nbsp;</div>
             </form>
         </div>
     </section>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Shopify</title>
+    <title>Collivery Shopify - @yield('title', 'Shipping')</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
