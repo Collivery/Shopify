b460188d7806f5e2ff7c0dd24a03f4f96b474e92
Peter Chaula <petercchaula@gmail.com>
2016-08-15 16:28:26 +0200

    Reformat auth middleware class - PSR-2 again

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,38 +1,38 @@
 <?php
 
-    namespace App\Http\Middleware;
+namespace App\Http\Middleware;
 
-    use Closure;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Auth;
+use Closure;
+use Illuminate\Support\Facades\Auth;
 
-    class Authenticate
+class Authenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @param  string|null              $guard
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
     {
-        /**
-         * Handle an incoming request.
-         *
-         * @param  \Illuminate\Http\Request $request
-         * @param  \Closure                 $next
-         * @param  string|null              $guard
-         *
-         * @return mixed
-         */
-        public function handle($request, Closure $next, $guard = null)
-        {
-            if (Auth::guard($guard)->guest()) {
-                if ($request->ajax() || $request->wantsJson()) {
-                    return response('Unauthorized.', 401);
-                }
 
-                $redirectUrl = 'login';
-
-                if ($request->path() !== 'login') {
-                    $redirectUrl .= '/?redirectUrl=' . $request->fullUrl();
-                }
-
-                return redirect()->guest($redirectUrl);
+        if (Auth::guard($guard)->guest()) {
+            if ($request->ajax() || $request->wantsJson()) {
+                return response('Unauthorized.', 401);
             }
 
-            return $next($request);
+            $redirectUrl = 'login';
+
+            if ($request->path() !== 'login') {
+                $request->session()->put('url.intended', $request->fullUrl());
+            }
+
+            return redirect()->guest($redirectUrl);
         }
+
+        return $next($request);
     }
+}
