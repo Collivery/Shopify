a4a93ab9dae165e856deda1a24448b2d5d328ea8
Peter Chaula <petercchaula@gmail.com>
2016-08-17 09:52:00 +0200

    Carrier service routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,4 +4,15 @@ Route::get('/', ['middleware' => 'auth', 'uses' => 'HomeController@index']);
 Route::post('/install', 'ShopController@requestPermissions');
 Route::get('/shop/setup', 'ShopController@setup');
 
+//webhook routes
+Route::post("/service/customers/create", "Hooks\WebhookController@customersCreate");
+Route::post("/service/customers/update", "Hooks\WebhookController@customersUpdate");
+Route::post("/service/orders/create", "Hooks\WebhookController@ordersCreate");
+Route::post("/service/orders/paid", "Hooks\WebhookController@ordersPaid");
+Route::post("/service/app/uninstalled", "Hooks\WebhookController@appUninstalled");
+Route::post("/service/shop/update", "Hooks\WebhookController@shopUpdate");
+
+//shipping rates
+Route::post("/service/shipping/rates", "Hooks\WebhookController@rates");
+
 Route::auth();
