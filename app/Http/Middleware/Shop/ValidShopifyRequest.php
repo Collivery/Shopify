efca3fe2d268b5d8945c8dc73075adb50cc0373d
Peter Chaula <petercchaula@gmail.com>
2016-08-17 09:50:51 +0200

    Shopify request verification

---
+++ app/Http/Middleware/Shop/ValidShopifyRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware\Shop;
+
+use Closure;
+
+class ValidShopifyRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $client = new \ShopifyClient(null, null, null, config('shopify.secret'));
+
+        if (!$client->validateSignature(array_slice($_GET, 0, 10))) {
+            return abort(400, 'Bad reqeust sent');
+        }
+
+        return $next($request);
+    }
+}
