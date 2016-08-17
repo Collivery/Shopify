2c0bdf889920631bc8990aa26471264689394b0f
Peter Chaula <petercchaula@gmail.com>
2016-08-17 09:54:23 +0200

    New shopify scopes

--- config/shopify.php
+++ config/shopify.php
@@ -6,7 +6,7 @@ return [
     'callback_uri'      => env('SHOPIFY_APP_CALLBACK_URI'),
     'domain'            => 'myshopify.com',
     'url'               => 'https://myshopify.com',
-    'scopes'            => 'read_customers,write_customers,read_orders,write_orders,read_script_tags,write_script_tags,read_fulfillments,write_fulfillments,read_shipping,write_shipping',
+    'scopes'            => 'read_customers,write_customers,read_orders,write_orders,read_script_tags,write_script_tags,read_fulfillments,write_fulfillments,read_shipping,write_shipping,read_products,write_products',
     'app_name'          => 'MDS Collivery Shipping',
     'shipping_endpoint' => env('APP_URL') . '/shipping/rates',
 ];
