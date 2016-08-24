7790dd7131049860cbfca10aa25fe8e061a9b383
Peter Chaula <petercchaula@gmail.com>
2016-08-24 17:37:54 +0200

    Reformat User model

--- app/User.php
+++ app/User.php
@@ -1,40 +1,40 @@
 <?php
 
-    namespace App;
+namespace App;
 
-    use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-    class User extends Authenticatable
+class User extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'plan',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function subscribed($plan = null)
     {
-        /**
-         * The attributes that are mass assignable.
-         *
-         * @var array
-         */
-        protected $fillable = [
-            'name',
-            'email',
-            'password',
-            'plan',
-        ];
-
-        /**
-         * The attributes that should be hidden for arrays.
-         *
-         * @var array
-         */
-        protected $hidden = [
-            'password',
-            'remember_token',
-        ];
-
-        public function subscribed($plan = null)
-        {
-            return $plan === $this->plan;
-        }
-
-        public function shops()
-        {
-            return $this->hasMany('App\Shop');
-        }
+        return $plan === $this->plan;
     }
+
+    public function shops()
+    {
+        return $this->hasMany('App\Shop');
+    }
+}
