ca75636b10196de2f17325f90879a7809e5e85bb
Peter Chaula <petercchaula@gmail.com>
2016-08-24 17:35:54 +0200

    Register default Collivery API resolver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,36 @@
 <?php
 
-    namespace App\Providers;
+namespace App\Providers;
 
-    use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider;
+use Mds\Collivery;
+use App\Helper\Resolver;
 
-    class AppServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
     {
-        /**
-         * Bootstrap any application services.
-         *
-         * @return void
-         */
-        public function boot()
-        {
-            //
-        }
-
-        /**
-         * Register any application services.
-         *
-         * @return void
-         */
-        public function register()
-        {
-            //
-        }
+        //
     }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('soap', function ($app) {
+            return new Collivery();
+        });
+
+        $this->app->singleton('resolver', function ($app) {
+            return new Resolver(app('soap'));
+        });
+    }
+}
