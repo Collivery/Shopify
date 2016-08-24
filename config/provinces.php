95358d631c54bbbeba05ac2aa18bfcfce206ae93
Peter Chaula <petercchaula@gmail.com>
2016-08-24 17:41:49 +0200

    Province mapping config file

---
+++ config/provinces.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'Eastern Cape'  => 'EC',
+
+    'Free State'    => 'OFS',
+
+    'Gauteng'       => 'GAU',
+
+    'KwaZulu-Natal' => 'KZN',
+
+    'Limpopo'       => 'NP',
+
+    'Mpumalanga'    => 'MP',
+
+    'North West'    => 'NW',
+
+    'Northern Cape' => 'NC',
+
+    'Western Cape'  => 'CAP',
+];
