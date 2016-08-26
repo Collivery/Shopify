7bffd335e23613700f5c4dfdc35d034a3d44fab2
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2016-08-26 17:36:15 +0200

    Indentation

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -11,15 +11,15 @@ use Validator;
 class AuthController extends Controller
 {
     /*
-        |--------------------------------------------------------------------------
-        | Registration & Login Controller
-        |--------------------------------------------------------------------------
-        |
-        | This controller handles the registration of new users, as well as the
-        | authentication of existing users. By default, this controller uses
-        | a simple trait to add these behaviors. Why don't you explore it?
-        |
-        */
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -8,15 +8,15 @@ use Illuminate\Foundation\Auth\ResetsPasswords;
 class PasswordController extends Controller
 {
     /*
-        |--------------------------------------------------------------------------
-        | Password Reset Controller
-        |--------------------------------------------------------------------------
-        |
-        | This controller is responsible for handling password reset requests
-        | and uses a simple trait to include this behavior. You're free to
-        | explore this trait and override any methods you wish to tweak.
-        |
-        */
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
 
     use ResetsPasswords;
 
