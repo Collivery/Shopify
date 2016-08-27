6f723b8c63ae3983dfb2aeed6b018708e88f8419
Peter Chaula <petercchaula@gmail.com>
2016-08-27 22:59:37 +0200

    Refactor shop controller

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -46,76 +46,18 @@ class ShopController extends Controller
         return redirect('/');
     }
 
-    public function requestPermissions(Request $request)
+    public function getShop(Request $request)
     {
-        if ($this->validateShop($request)) {
-            //disable existing installation
-            DB::table('shops')->where([
-                'shop' => Input::get('shop'),
-                'app_installed' => 1,
-            ])->update([
-                'app_installed' => 2,
-            ]);
-
-            $shop = new Shop();
-
-            $shop->shop = Input::get('shop');
-            $user = Auth::user();
-            $shop->user_id = $user['id'];
-            $shop->nonce = sha1(str_random(64));
-
-            if ($shop->save()) {
-                return $this->redirectToShopAdmin($shop);
-            }
-        }
-
-        $request->session()->flash('shop_error', 'Invalid shop url');
-
-        $queryString = !Input::get('shop') ? '' : '?shop='.urlencode(Input::get('shop'));
-
-        return redirect('/'.$queryString);
-    }
-
-    private function validateShop(Request $request)
-    {
-        $shop = $request->input('shop');
-        $server = config('shopify.domain');
-
-        return preg_match("|[a-z\-\d]{3,100}\.${server}|", $shop) === 1;
-    }
-
-    private function redirectToShopAdmin(Shop $shop)
-    {
-        $client = new \ShopifyClient($shop->shop, null, config('shopify.api_key'), config('shopify.secret'));
-        $installUrl = $client->getAuthorizeUrl(config('shopify.scopes'), config('shopify.redirect_uri'));
-        $installUrl .= '&state='.urlencode($shop->nonce);
-
-        return redirect($installUrl);
+        return Shop::where('shop', $request->input('shop'))->where('nonce', $request->input('state'))->first();
     }
 
     private function registerWebhooks(Shop $shop)
     {
         $client = $this->getShopifyClient($shop);
 
-        $payload = [
-            'name' => config('shopify.app_name'),
-            'callback_url' => config('shopify.shipping_endpoint'),
-            'service_discovery' => true,
-        ];
-
-        if (config('app.debug')) {
-            $payload['callback_url'] = env('NGROK_URL').'/service/shipping/rates';
-        }
-
         $service = null;
         try {
-            $service = $client->call('POST', '/admin/carrier_services.json', [
-                'carrier_service' => $payload,
-            ]);
-            $shop->carrier_id = $service['id'];
-            $shop->carrier_installed = 1;
-            $shop->carrier_installed_on = Carbon::now();
-
+            $this->registerCarrier($service, $client, $shop);
             //register webhooks
             $webhooks = json_decode(file_get_contents(resource_path('json/webhooks.json')), true);
             $hookDomain = config('app.url');
@@ -162,8 +104,77 @@ class ShopController extends Controller
         return false;
     }
 
-    public function getShop(Request $request)
+    /**
+     * @param                $service
+     * @param \ShopifyClient $client
+     * @param Shop           $shop
+     *
+     * @return bool
+     */
+    private function registerCarrier(&$service, \ShopifyClient $client, Shop $shop)
     {
-        return Shop::where('shop', $request->input('shop'))->where('nonce', $request->input('state'))->first();
+        $payload = [
+            'name' => config('shopify.app_name'),
+            'callback_url' => config('shopify.shipping_endpoint'),
+            'service_discovery' => true,
+        ];
+
+        if (config('app.debug')) {
+            $payload['callback_url'] = env('NGROK_URL').'/service/shipping/rates';
+        }
+
+        $service = $client->call('POST', '/admin/carrier_services.json', [
+            'carrier_service' => $payload,
+        ]);
+        $shop->carrier_id = $service['id'];
+        $shop->carrier_installed = 1;
+        $shop->carrier_installed_on = Carbon::now();
+    }
+
+    public function requestPermissions(Request $request)
+    {
+        if ($this->validateShop($request)) {
+            //disable existing installation
+            DB::table('shops')->where([
+                'shop' => Input::get('shop'),
+                'app_installed' => 1,
+            ])->update([
+                'app_installed' => 2,
+            ]);
+
+            $shop = new Shop();
+
+            $shop->shop = Input::get('shop');
+            $user = Auth::user();
+            $shop->user_id = $user['id'];
+            $shop->nonce = sha1(str_random(64));
+
+            if ($shop->save()) {
+                return $this->redirectToShopAdmin($shop);
+            }
+        }
+
+        $request->session()->flash('shop_error', 'Invalid shop url');
+
+        $queryString = !Input::get('shop') ? '' : '?shop='.urlencode(Input::get('shop'));
+
+        return redirect('/'.$queryString);
+    }
+
+    private function validateShop(Request $request)
+    {
+        $shop = $request->input('shop');
+        $server = config('shopify.domain');
+
+        return preg_match("|[a-z0-9\-]{3,100}\.${server}|", $shop) === 1;
+    }
+
+    private function redirectToShopAdmin(Shop $shop)
+    {
+        $client = new \ShopifyClient($shop->shop, null, config('shopify.api_key'), config('shopify.secret'));
+        $installUrl = $client->getAuthorizeUrl(config('shopify.scopes'), config('shopify.redirect_uri'));
+        $installUrl .= '&state='.urlencode($shop->nonce);
+
+        return redirect($installUrl);
     }
 }
