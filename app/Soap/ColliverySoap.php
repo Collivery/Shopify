41cebe1c008e1f20fd11fe324e4562d7f536e183
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2016-09-02 10:55:12 +0200

    Custom Collivery client for authenticating users against collivery

---
+++ app/Soap/ColliverySoap.php
@@ -0,0 +1,33 @@
+<?php
+
+use Mds\Collivery;
+
+class ColliverySoap extends Collivery
+{
+    public function __construct(array $config = [])
+    {
+        if (empty($config['cache'])) {
+            $config['cache'] = storage_path().'/collivery';
+        }
+        parent::__construct($config);
+    }
+
+    public function verify($username, $password)
+    {
+        try {
+            $this->client->authenticate($username, $password, null,
+                [
+                    'name' => $this->config->app_name.' mds/collivery/class',
+                    'version' => $this->config->app_version,
+                    'host' => $this->config->app_host,
+                    'url' => $this->config->app_url,
+                    'lang' => 'PHP '.phpversion(),
+                ]);
+
+            return true;
+        } catch (\SoapFault $e) {
+        }
+
+        return false;
+    }
+}
