6f7947738246308edb98b38c812296c5cd7fe239
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2016-09-02 11:41:47 +0200

    Custom order finder

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -6,4 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    public function scopeById($query, $id)
+    {
+        return $query->where('shopify_order_id', $id);
+    }
 }
